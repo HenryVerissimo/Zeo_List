11bbc28a476292f3c764af34fb953e2b2699e57c
Henrique Verissimo <henriquedevsp@gmail.com>
2025-08-18 19:43:37 -0300

    feat: creating the main classes of the project
    - add the main class to the project.
    - add the MyApp class to the project.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:client/app/View/pages/login_page.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      routes: {"/": (context) => LoginPage()},
+    );
+  }
+}
