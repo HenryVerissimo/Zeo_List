af280919156b050d2fa949b22a5372e488370405
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-08-24 02:17:53 -0300

    feat(screen): lock horizontal screen
    - User can only use the app vertically.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:client/app/View/pages/login/create_account_page.dart';
 import 'package:client/app/View/pages/login/login_page.dart';
+import 'package:client/app/View/pages/login/reset_password_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   runApp(MyApp());
 }
 
@@ -17,7 +24,11 @@ class MyApp extends StatelessWidget {
         primaryColor: Color.fromRGBO(64, 123, 255, 1),
         scaffoldBackgroundColor: Color.fromRGBO(30, 30, 30, 1),
       ),
-      routes: {"/": (context) => LoginPage(), "/create_account": (context) => CreateAccountPage()},
+      routes: {
+        "/": (context) => LoginPage(), 
+        "/create_account": (context) => CreateAccountPage(), 
+        "/reset_password": (context) => ResetPasswordPage()
+      },
       initialRoute: "/",
     );
   }
