e7293250512c767fa186eb091450c42012728ac9
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-08-24 02:23:34 -0300

    feat(reset password): create a new page in the project
    - add ResetPasswordPage for users to recover their accounts.
    - - adds navigation between the login and password reset pages.

---
+++ client/lib/app/View/pages/login/reset_password_page.dart
@@ -0,0 +1,30 @@
+import 'package:client/app/View/widgets/custom_button_github.dart';
+import 'package:client/app/View/widgets/custom_image_text_widget.dart';
+import 'package:client/app/View/widgets/login/card_reset_password_widget.dart';
+import 'package:flutter/material.dart';
+
+class ResetPasswordPage extends StatelessWidget{
+  const ResetPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: CustomImageTextWidget(text: "ZEO LIST", image: "assets/images/reset_password_ilustracao.svg"),
+            ),
+            CardResetPasswordWidget(),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: CustomButtonGithub(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- client/lib/app/View/widgets/login/card_reset_password_widget.dart
+++ client/lib/app/View/widgets/login/card_reset_password_widget.dart
@@ -3,11 +3,11 @@ import 'package:client/app/View/widgets/login/textbutton_login_widget.dart';
 import 'package:client/app/View/widgets/login/textfield_login_widget.dart';
 import 'package:flutter/material.dart';
 
-class CardLoginWidget extends StatelessWidget {
-  const CardLoginWidget({super.key});
+class CardResetPasswordWidget extends StatelessWidget{
+  const CardResetPasswordWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Container(
       width: 350,
       height: 300,
@@ -27,14 +27,14 @@ class CardLoginWidget extends StatelessWidget {
             TextfieldWidgetLogin(text: "Email", obscureText: false),
             Padding(
               padding: const EdgeInsets.only(top: 20),
-              child: TextfieldWidgetLogin(text: "Senha", obscureText: true),
+              child: TextfieldWidgetLogin(text: "Código", obscureText: true),
             ),
             SizedBox(
               width: 320,
               child: Padding(
                 padding: const EdgeInsets.only(top: 2, bottom: 2),
                 child: CustomButtonWidget(
-                  text: "LOGIN",
+                  text: "REENVIAR EMAIL",
                   func: () {},
                   bgColor: Theme.of(context).primaryColor,
                 ),
@@ -45,15 +45,28 @@ class CardLoginWidget extends StatelessWidget {
               height: 2,
               color: const Color.fromARGB(117, 0, 0, 0),
             ),
+            SizedBox(
+              width: 320,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 2, bottom: 2),
+                child: CustomButtonWidget(
+                  text: "CONFIRMAR CÓDIGO",
+                  func: () {},
+                  bgColor: Colors.green,
+                ),
+              ),
+            ),
+            Container(
+              width: 320,
+              height: 2,
+              color: const Color.fromARGB(117, 0, 0, 0),
+            ),
             CustomTextbuttonWidget(
-              text: "Não tem uma conta? cadastre-se",
+              text: "Voltar para o login",
               func: () {
-                Navigator.pushReplacementNamed(context, "/create_account");
+                Navigator.pushReplacementNamed(context, "/");
               },
             ),
-            CustomTextbuttonWidget(text: "Esqueci minha senha", func: () {
-              Navigator.pushReplacementNamed(context, "/reset_password");
-            }),
           ],
         ),
       ),
