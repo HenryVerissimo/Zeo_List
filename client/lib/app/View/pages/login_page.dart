0bae0f74807c99ded514a005f4217b3e10c25afb
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-08-23 21:02:19 -0300

    refactor(page): Convert LoginPage to StateLessWidget

--- client/lib/app/View/pages/login_page.dart
+++ client/lib/app/View/pages/login_page.dart
@@ -3,15 +3,10 @@ import 'package:client/app/View/widgets/custom_logo_projeto_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
   final Uri urlGitHub = Uri.parse("https://github.com/HenryVerissimo/Zeo_List");
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
